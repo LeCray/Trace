2aa99a27a2a1243e64ed74666022d78303bc3214
LeCray <mistercryptogrid@gmail.com>
2017-11-06 16:58:51 +0200

    Added the Car model and controller

---
+++ db/migrate/20171106145756_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :vehicle_reg
+
+      t.timestamps
+    end
+  end
+end
