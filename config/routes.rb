6beeda3dea52d794d22836783932d53bbfbc4e46
LeCray <mistercryptogrid@gmail.com>
2017-12-04 09:02:47 +0200

    About to build password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  	
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	get '/login' => 'sessions#new'
 	post '/login' => 'sessions#create'
