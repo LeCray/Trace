45f627e5a412b97052b0c3d9f77e51156eb753d8
LeCray <mistercryptogrid@gmail.com>
2017-12-13 08:40:04 +0200

    Added redis init

--- Gemfile
+++ Gemfile
@@ -67,7 +67,7 @@ gem 'mailgunner', '~> 2.4'
 gem 'will_paginate-bootstrap'
 
 gem 'redis', '~> 3.3'
-gem 'redis-store'
+
 
 
 gem 'coffee-script-source', '~> 1.8.0'
---
+++ config/initializers/redis.rb
@@ -0,0 +1,1 @@
+$redis = Redis.new(url: "redis://h:p3ee00d92e393bd6a1577eb0aff5436b0be365982c396a26c92924232fb3fc8cc@ec2-34-239-85-133.compute-1.amazonaws.com:45879")
