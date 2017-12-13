c4739516600b6246e3d910842eaa698b393d997c
LeCray <mistercryptogrid@gmail.com>
2017-12-13 16:04:48 +0200

    Added a missing bracket

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 require 'redis'
 
-uri = URI.parse(ENV["REDISCLOUD_URL"]
+uri = URI.parse(ENV["REDISCLOUD_URL"])
 #redis = Redis.new(url: "redis://rediscloud:password@localhost:6379")
 REDIS = Redis.new(:host => uri.host, :port => uri.port)
