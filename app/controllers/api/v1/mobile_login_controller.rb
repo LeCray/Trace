36a6f06b9d4c72fab6912250ff106848ae72e5d9
LeCray <mistercryptogrid@gmail.com>
2017-12-27 21:40:26 +0200

    adding mobile api

--- app/controllers/api/v1/mobile_login_controller.rb
+++ app/controllers/api/v1/mobile_login_controller.rb
@@ -2,10 +2,33 @@ module Api
 	module V1
 		class MobileLoginController < ApplicationController
 			skip_before_action :verify_authenticity_token
+			before_action :verify_params
 		
 
-			def create	
+			def mobile_login	
+				email  		= params[:email]
+				password    = params[:password]
 
+				admin = Admin.find_by(email: email)
+				driver = Driver.find_by(email: password)
+
+				if admin && admin.authenticate(password)
+					admin_log_in admin
+					render json: { authenticated: true }
+				elsif driver && driver.authenticate(password)
+					driver_log_in driver
+					render json: { authenticated: true }
+				else
+					render json: {authenticated: false, email: email}
+				end
+			end
+
+			def verify_params
+				if !params[:email]
+					render json: {MSG: "No email"}
+				end
+
+					
 			end
 
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
 
 	namespace :api do
 		namespace :v1 do
-			post '/mobile_login' => 'mobile_login#create'
+			post '/mobile_login' => 'mobile_login#mobile_login'
 		end
 	end
 
