771f3067339c50e4c492834186266c0df2c48eb8
LeCray <mistercryptogrid@gmail.com>
2018-03-23 01:12:49 +0200

    Precompiled assets

---
+++ public/assets/drivers/Bookings-1d371b978c89a97f8b7fb81084d5cefa8135a1e876a661fba0984ebc759416fd.js
@@ -0,0 +1,71 @@
+var Bookings = (function() {
+
+	var $btnConfirm;
+	var $btnCancel;
+	var $email;
+	var $parameters;
+	var confirm_url = '/api/v1/bookings/confirm_booking';
+	var cancel_url = '/api/v1/bookings/cancel_booking';
+
+
+	var fetchElements = function() {
+		$parameters 			= $('#parameters');
+		$email 					= $parameters.data('email');
+		$btnConfirm       		= $('#btnConfirm');
+		$btnCancel 				= $('#btnCancel');
+	};
+
+
+	var initializeEvents = function() {
+
+		$btnConfirm.on("click", function() {
+			var email = $email;
+			console.log("Booking Confirmed. Email: " + email )//+ ", DriverID: " + driver_id + ", Booking: " + booking);		
+
+			$.ajax({
+				url: confirm_url,
+				method: 'POST',
+				dataType: 'json',
+				data: {		
+					email: email,
+				},
+
+				success: function(response) {
+					 document.getElementById("status").innerHTML = "CONFIRMED"
+				}
+			});
+		});
+
+		$btnCancel.on("click", function() {
+			var email = $email;
+			console.log("Booking Cancelled. Email: " + email )//+ ", DriverID: " + driver_id + ", Booking: " + booking);		
+
+			$.ajax({
+				url: cancel_url,
+				method: 'POST',
+				dataType: 'json',
+				data: {		
+					email: email,
+				},
+
+				success: function(response) {
+					document.getElementById("status").innerHTML = "CANCELLED"
+				}
+			});
+		});
+
+	};
+
+	var init = function() {
+		fetchElements();
+		initializeEvents();
+	};
+
+	return {
+		init: init 
+	};
+})();
+
+
+
+
