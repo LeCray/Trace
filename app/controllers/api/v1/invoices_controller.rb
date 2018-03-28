c62743e5797e1cba21a6f990c18b34d9d1eb460a
LeCray <mistercryptogrid@gmail.com>
2018-03-28 23:18:00 +0200

    Returns list of drivers invoices

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -11,9 +11,14 @@ module Api
 				@invoices = @driver.invoices.all
 
 				invoices = []
+				inv_url = []
+				inv_date = []
+
+				invoices.push(inv_url, inv_date)
 
 				@invoices.each do |i|
-					invoices.push(i.attachment_url)
+					inv_url.push(i.attachment_url)
+					inv_date.push(i.created_at)
 				end
 	
 
