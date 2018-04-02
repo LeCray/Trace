c1131ab168c609358d19e6d4de6c5cae949556d2
LeCray <mistercryptogrid@gmail.com>
2018-04-02 03:01:25 +0200

    Added quo status to get quotation

--- app/controllers/api/v1/quotations_controller.rb
+++ app/controllers/api/v1/quotations_controller.rb
@@ -13,15 +13,17 @@ module Api
 				quo_url 	= []
 				quo_date 	= []
 				quo_id 		= []
+				quo_status 	= []
 
 				@quotations.each do |q|
 					quo_url.push(q.attachment_url)
 					quo_date.push(q.created_at)
 					quo_id.push(q.id)
+					quo_status.push(q.status)
 				end
 
 				quotations 	= []
-				quotations.push(quo_url, quo_date, quo_id)
+				quotations.push(quo_url, quo_date, quo_id, quo_status)
 	
 				render json: {quotations: quotations}						
 			end  
